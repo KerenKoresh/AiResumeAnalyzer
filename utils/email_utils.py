480a6bd71ec15794340ef9109f9f315dab28cdce
keren <kwewnkoresh1@gmail.com>
2025-05-02 15:56:28 +0300

    email fix

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -2,33 +2,31 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import logging
+import streamlit as st
 
 logger = logging.getLogger("AIResumeAnalyzer")
 
 
+FROM_EMAIL = st.secrets["FROM_EMAIL"]
+EMAIL_PASSWORD = st.secrets["EMAIL_PASSWORD"]
+
 def send_email(subject, body, to_email):
-    from_email = "ai.resume.analyzer.ko@gmail.com"  # כתובת המייל שלך
-    from_password = "icos ofqm bvqd iwdz"  # סיסמא של המייל שלך
-
-    # הגדרת השרת של Gmail
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-
     try:
-        # התחברות לשרת
-        server.login(from_email, from_password)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(FROM_EMAIL, EMAIL_PASSWORD)
 
-        # יצירת המייל
         msg = MIMEMultipart()
-        msg['From'] = from_email
+        msg['From'] = FROM_EMAIL
         msg['To'] = to_email
         msg['Subject'] = subject
-
         msg.attach(MIMEText(body, 'plain'))
 
-        server.sendmail(from_email, to_email, msg.as_string())
+        server.sendmail(FROM_EMAIL, to_email, msg.as_string())
         server.quit()
+
         logger.info(f"🔔 Email sent successfully to {to_email}")
         st.success(f"Results have been sent to {to_email}")
     except Exception as e:
         logger.error(f"Error sending email: {e}")
+        st.error("❌ Failed to send email.")
