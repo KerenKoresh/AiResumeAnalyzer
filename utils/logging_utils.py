2ce5bee504813845c6623c3fd733c9efa9fabbf2
keren <kwewnkoresh1@gmail.com>
2025-05-02 10:22:23 +0300

    load_dotenv

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,9 +1,13 @@
 import os
 import logging
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()  # זה יטען את משתני הסביבה מקובץ .env
 
 
 class BetterStackHandler(logging.Handler):
+
     def __init__(self, url, token):
         super().__init__()
         self.url = url
@@ -19,13 +23,12 @@ class BetterStackHandler(logging.Handler):
             "Content-Type": "application/json"
         }
 
-        # אם ה-URL לא כולל סכימה, נוסיף אותה
         if not self.url.startswith(('http://', 'https://')):
             self.url = 'https://' + self.url
 
         try:
             response = requests.post(self.url, json=payload, headers=headers)
-            response.raise_for_status()  # יזרוק חריגה אם הסטטוס לא 200
+            response.raise_for_status()
         except requests.exceptions.RequestException as e:
             print(f"Error sending log to BetterStack: {e}")
 
@@ -34,13 +37,11 @@ def add_betterstack_handler():
     source_token = os.getenv("SOURCE_TOKEN")
     host = os.getenv("HOST")
 
-    # ודא שהמשתנים קיימים
     if not source_token or not host:
         raise ValueError("SOURCE_TOKEN or HOST is not set in the environment variables.")
 
     handler = BetterStackHandler(url=host, token=source_token)
 
-    # קבלת הלוגר מהמקום שבו הוא מוגדר
     logger = logging.getLogger('betterstack_logger')
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
