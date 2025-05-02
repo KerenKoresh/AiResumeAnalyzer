d78acf43287032f2a913d2d3400c4feb04ed63b6
keren <kwewnkoresh1@gmail.com>
2025-05-02 11:09:49 +0300

    st.secrets

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ from utils.email_utils import send_email  # ייבוא הפונקציה לשלי
 # הגדרת Streamlit (רק פעם אחת)
 st.set_page_config(page_title="AI Resume Analyzer", layout="centered")
 
-
-@st.cache_resource
 def init_logger():
     add_betterstack_handler()
     return True
 
-
+# אתחול הלוגינג
 init_logger()
 
 # הגדרת Streamlit
@@ -38,8 +36,7 @@ if st.button("🔍 Analyze match"):
                 resume_text = extract_text_from_pdf(uploaded_file)
 
                 if not resume_text.strip():
-                    st.error(
-                        "No text found in the PDF file. Please ensure the file is valid and not scanned as an image.")
+                    st.error("No text found in the PDF file. Please ensure the file is valid and not scanned as an image.")
                 else:
                     # ניתוח חדש
                     result = analyze_match(resume_text, job_description)
